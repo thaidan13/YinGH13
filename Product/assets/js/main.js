c399ed7e29a9b2dd1a797dbbc108dd3a0d2c4eb1
Thái Dân <103986347+thaidan13@users.noreply.github.com>
2023-07-03 17:58:25 +0700

    ok

--- Product/assets/js/main.js
+++ Product/assets/js/main.js
@@ -182,3 +182,40 @@ mic.onclick = () => {
         mic.classList.remove('record');
     }
 }
+
+
+// Cart
+
+let list = document.querySelectorAll(".pro-container .pro");
+list.forEach(item => {
+    item.addEventListener('click', function (event) {
+        if (event.target.classList.contains('add')) {
+            var itemNew = item.cloneNode(true);
+            let checkIsset = false;
+
+            let listCart = document.querySelectorAll("#cart .pro");
+            listCart.forEach(cart => {
+                if (cart.getAttribute('data-key') == itemNew.getAttribute('data-key')) {
+                    checkIsset = true;
+                    cart.classList.add('danger');
+                    setTimeout(function () {
+                        cart.classList.remove('danger');
+                    }, 1000)
+                }
+            })
+            if (checkIsset == false) {
+                document.querySelector(".listCart").appendChild(itemNew);
+            }
+
+        }
+    })
+})
+function Remove($key) {
+    let listCart = document.querySelectorAll("#cart .pro");
+    listCart.forEach(item => {
+        if (item.getAttribute('data-key') == $key) {
+            item.remove();
+            return;
+        }
+    })
+}
